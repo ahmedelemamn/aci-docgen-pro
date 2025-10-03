8c3f16b7c9f6df55811c306ed49b14f1c94cb217
Ahmed Elemam <ahmedelemamn@gmail.com>
2025-10-03 19:36:20 +1000

    Enhance contract harvesting details

--- aci_docgen/utils/normalize.py
+++ aci_docgen/utils/normalize.py
@@ -1,5 +1,25 @@
 def simple_attr(mo, cls):
     return mo.get(cls, {}).get('attributes', {})
 
+
 def collect_children(mo, cls):
     return [c[cls] for c in [x for x in mo.get('children', []) if cls in x]]
+
+
+def sorted_unique(values):
+    """Return a sorted list of unique, truthy values."""
+
+    if not values:
+        return []
+
+    seen = set()
+    filtered = []
+    for value in values:
+        if not value:
+            continue
+        if value in seen:
+            continue
+        seen.add(value)
+        filtered.append(value)
+
+    return sorted(filtered, key=lambda v: str(v))
